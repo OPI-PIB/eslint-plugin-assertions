da30ee4ba350272c42d5b6e1d1504e608cf00387
Dawid Lubowiecki <dlubowiecki@opi.org.pl>
2022-11-18 14:22:19 +0100

    v1.0.3

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,14 @@ module.exports = {
 	forceExit: true,
 	verbose: true,
 	collectCoverage: false,
-	globals: {
-		'ts-jest': {
-			tsconfig: 'tsconfig.json',
-		},
+	transform: {
+		'^.+\\.(js|ts|tsx)$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.json',
+			},
+		],
 	},
 	moduleFileExtensions: ['ts', 'js'],
-	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest',
-	},
 	testMatch: ['./**/*.spec.(ts|js)'],
 };
